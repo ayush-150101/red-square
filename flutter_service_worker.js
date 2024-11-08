6e745b6f35706960d0ddcbb3f05f2f5a6b0c1a71
ayush-150101 <57745707+ayush-150101@users.noreply.github.com>
2024-11-08 11:52:30 +0530

    web build files init

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -18,8 +18,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "570754cc3e8ecb250fb8d749833e3e5c",
-"/": "570754cc3e8ecb250fb8d749833e3e5c",
+"index.html": "b0bc4abe64496fcfc632c6f935c21e1d",
+"/": "b0bc4abe64496fcfc632c6f935c21e1d",
 "main.dart.js": "019cd624e3ebbe0b4ed84c5cf49308a5",
 "manifest.json": "b988fddc436ec998b5dca3a4e242049a",
 "version.json": "0fe678c4b5754e7556bc1a66eab890aa"
